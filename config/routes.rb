5fac6db520a83e656cd2f64d92d944c4ce53283a
Susan Prestage <susan@sportsrocket.com>
2019-04-05 14:00:57 -0500

    Add resume support

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+
+  get ':action' => 'static#:action'  # replace 'match' with 'get' for rails4
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
